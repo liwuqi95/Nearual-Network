d69da9140bbdf36571f1468ac33b0599703a36fc
liwuqi95 <wuqi.li@mail.utoronto.ca>
2018-04-05 12:47:26 -0400

    minor fix

--- 1.1.py
+++ 1.1.py
@@ -217,7 +217,7 @@ if plot:
 	plt.figure(2)
 	plt.plot(epoch_list, loss_list,'-', label = "learning_rate is " + str(learning_rate))
 	plt.plot(epoch_list, loss_array[0],'-', label = "learning_rate is 0.0001")
-	plt.plot(epoch_list, loss_array[1],'-', label = "learning_rate is 0.05")
+	plt.plot(epoch_list, loss_array[1],'-', label = "learning_rate is 0.001")
 
 	plt.xlabel('number of epochs')
 	plt.ylabel('cross entropy loss')
